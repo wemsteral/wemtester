51ee392065307d5a17707d82a319cd8ba2c08366
Alasdair <alasdair123@hotmail.co.uk>
2019-05-16 10:40:33 +0100

    commit project 16.05.19

---
+++ src/matchers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  identical: require("./identical"),
+  same: require("./same"),
+  truthy: require("./truthy"),
+  falsy: require("./falsy"),
+  throws: require("./throws"),
+  deeplyIdentical: require("./deeply-identical")
+};
